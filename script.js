de3ee5682610153f171ca42bd2d0f2caf7c599f6
Nicholas Gersbach <54191233+Nick-Gersbach@users.noreply.github.com>
2023-06-08 09:26:22 -0700

    back to top button added

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //Mobile Menu Fucntionality
 document.querySelector('.menu-btn').addEventListener('click', () => document.querySelector('ul').classList.toggle('show'));
+
+
+//Back to top functionality
+const backToTop = document.querySelector('.back-to-top');
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    backToTop.classList.add("active");
+  } else {
+    backToTop.classList.remove("active");
+  }
+})
